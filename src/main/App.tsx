a2b58b64bb9f524aee72a1fac2cbfa4ef9d69fc1
AbdAllah <abdallah.a.elfatah@gmail.com>
2024-12-11 15:10:13 +0200

    init commit for login with google

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,8 +1,40 @@
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+
+const LoginWithGoogle = () => {
+  return (
+    <GoogleLogin
+      onSuccess={(credentialResponse) => {
+        const decodedToken = jwtDecode(credentialResponse.credential || "");
+        console.log(`Login Success: ${JSON.stringify(decodedToken)}`);
+      }}
+      onError={() => {
+        console.log("Login Failed");
+      }}
+    />
+  );
+};
+
+const LogoutFromGoogle = () => {
+  return (
+    <p
+      onClick={() => {
+        console.log("Logout");
+        googleLogout();
+      }}
+    >
+      Logout
+    </p>
+  );
+};
+
 function App() {
   return (
     <>
       <div>
         <p>ibn el-rabi</p>
+        <LoginWithGoogle />
+        <LogoutFromGoogle />
       </div>
     </>
   );
--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_LOGIN_CLIENT_ID}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>
 );
